746d6aff8a03267f32df49176344b52431307e23
Louis King <jinglemansweep@gmail.com>
2023-02-13 20:47:46 +0000

    More refactoring before initial push

--- wideboy/utils/helpers.py
+++ wideboy/utils/helpers.py
@@ -1,7 +1,6 @@
 import aiohttp
 import async_timeout
 import asyncio
-import feedparser
 import logging
 import os
 import pathlib
@@ -14,11 +13,10 @@ from wideboy.config import DEBUG, FT_SIZE
 logger = logging.getLogger(__name__)
 
 
-def intro_debug(name: str) -> None:
+def intro_debug() -> None:
     logger.info("=" * 80)
     logger.info(f"{_APP_DESCRIPTION} [{_APP_NAME}] v{_APP_VERSION}")
     logger.info("=" * 80)
-    logger.info(f"Widget: {name}")
     logger.info(f"Debug: {DEBUG}")
     logger.info(f"FT Panel Size: {FT_SIZE[0]}x{FT_SIZE[1]}")
     logger.info("=" * 80)
@@ -34,25 +32,11 @@ async def async_fetch(session: aiohttp.ClientSession, url: str) -> str:
             return await response.text()
 
 
-async def get_rss_items(loop: asyncio.AbstractEventLoop, url) -> Any:
-    feed = None
-    async with aiohttp.ClientSession(loop=loop) as session:
-        html = await async_fetch(session, url)
-        feed = feedparser.parse(html)
-    return feed
-
-
-def get_widget_name_from_path(script_filename: str) -> str:
-    full_path = os.path.dirname(os.path.realpath(script_filename))
-    path = pathlib.PurePath(full_path)
-    return path.name
-
-
 def get_config_env_var(
-    key: str, default: Any = None, widget: Optional[str] = None
+    key: str, default: Any = None, namespace: Optional[str] = None
 ) -> Optional[str]:
-    if widget is not None:
-        key = f"{widget}_{key}".upper()
+    if namespace is not None:
+        key = f"{namespace}_{key}".upper()
     value = os.environ.get(key, default)
     logger.debug(f"config:env key={key} value={value}")
     return value
