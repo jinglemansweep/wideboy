3fbeef7f292238efdb3714d99f2c8d77dddf5e7f
Louis King <jinglemansweep@gmail.com>
2023-02-15 15:42:45 +0000

    Stash

--- wideboy/utils/display.py
+++ wideboy/utils/display.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pygame
+import sys
 from typing import Any
 from PIL import Image
 from rgbmatrix import RGBMatrix  # type: ignore
 
-from wideboy.config import MATRIX_SIZE, MATRIX_PANEL_SIZE, matrix_options
+from wideboy.config import CANVAS_SIZE, MATRIX_SIZE, MATRIX_PANEL_SIZE, matrix_options
 
 
 def setup_led_matrix() -> tuple[RGBMatrix, Any]:
@@ -12,22 +13,74 @@ def setup_led_matrix() -> tuple[RGBMatrix, Any]:
     buffer = matrix.CreateFrameCanvas()
     return matrix, buffer
 
+
 def render_led_matrix(
     matrix: RGBMatrix, surface: pygame.surface.Surface, buffer: Any
 ) -> Any:
-    temp_surface = wrap_surface(
-        surface, MATRIX_SIZE, MATRIX_PANEL_SIZE
-    )  # numpy arrays might be faster than pygame blitting
-    #temp_surface = surface
-    #pixels = np.fliplr(np.rot90(pygame.surfarray.pixels3d(temp_surface), -1))
-    pixels = pygame.surfarray.pixels3d(temp_surface)
+    wrapped = wrap_surface(surface, MATRIX_SIZE, MATRIX_PANEL_SIZE)
+    pixels = pygame.surfarray.pixels3d(wrapped)
     image = Image.fromarray(pixels)
     buffer.SetImage(image)
     # Flip and return next buffer
     return matrix.SwapOnVSync(buffer)
 
 
+def wrap_surface_array(
+    array: np.ndarray,
+    canvas_shape: tuple[int, int],
+) -> np.array:
+    # FIXME: VERY BROKEN
+    wrapped = np.ndarray((canvas_shape[0], canvas_shape[1], 3), dtype=np.uint8)
+    print(f"array={array.shape} wrapped={wrapped.shape}")
+
+    wrapped = np.append(wrapped, array[0:256, 0:64, 0:3], axis=1)
+    # wrapped[64:0, 128:256] = array[0:256, 64:512]
+    # wrapped[128:0, 128:256] = array[0:256, 64:768]
+    return wrapped
+    """
+    width = array.shape[0] // canvas_shape[0]
+    row_height = array.shape[1]
+    print(f"width={width} row_height={row_height}")
+    row = 0
+    while row < width:
+        xa = row * canvas_shape[0]
+        ya = 0
+        xb = xa + canvas_shape[0]
+        yb = row_height
+        slice = array[ya:xa, yb:xb]
+        print(row, "SLICE", slice.shape, wrapped.shape)
+        # print(f"row={row}: slice={xa}:{ya} -> {xb}:{yb}")
+        wrapped = np.append(wrapped, slice)
+        row += 1
+    print(wrapped.shape)
+    return array
+    """
+
+
 def wrap_surface(
+    surface: pygame.surface.Surface,
+    new_shape: tuple[int, int],
+    tile_size: tuple[int, int],
+) -> pygame.surface.Surface:
+    temp_surface = pygame.Surface(new_shape)
+    surface_width = surface.get_rect().width
+    surface_height = surface.get_rect().height
+    wrapped_width = new_shape[0]
+    wrapped_row_count = surface_width // wrapped_width
+    row = 0
+    while row <= wrapped_row_count:
+        y = row * surface_height
+        x = wrapped_width * row
+        temp_surface.blit(
+            surface,
+            (0, y),
+            (x, 0, x + wrapped_width, surface_height),
+        )
+        row += 1
+    return temp_surface
+
+
+def wrap_surface_orig(
     surface: pygame.surface.Surface,
     shape: tuple[int, int],
     tile_size: tuple[int, int],
