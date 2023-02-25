759edf4e1125aee9a48315a389eccedff9bcddf5
Louis King <jinglemansweep@gmail.com>
2023-02-25 15:50:37 +0000

    Fixed brightness

--- wideboy/__main__.py
+++ wideboy/__main__.py
@@ -101,9 +101,7 @@ async def start_main_loop():
             pygame.display.update(updates)
 
         if MATRIX_ENABLED:
-            matrix_buffer = render_led_matrix(
-                matrix, screen, matrix_buffer, state.brightness
-            )
+            matrix_buffer = render_led_matrix(matrix, screen, matrix_buffer)
 
         loop_debug(frame, clock, delta, state)
         mqtt.loop(0.003)
