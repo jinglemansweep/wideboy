d768fee0a35de3657b6ba224fb0f0d4c5fcdb576
Louis King <jinglemansweep@gmail.com>
2023-04-07 14:34:53 +0100

    Tidying

--- wideboy/utils/logger.py
+++ wideboy/utils/logger.py
@@ -2,6 +2,9 @@ import logging
 
 LOG_FORMAT = "%(name)-20s %(levelname)-7s %(message)s"
 
+logging.getLogger("homeassistant_api").setLevel(logging.WARNING)
+logging.getLogger("requests_cache").setLevel(logging.WARNING)
+
 
 def setup_logger(level: int = logging.INFO) -> None:
     log_level = logging.DEBUG if level == "debug" else logging.INFO
