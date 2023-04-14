abea2fe3ecaa3980b24e35c50d9af77ab42020b6
Louis King <jinglemansweep@gmail.com>
2023-04-14 21:20:35 +0100

    Improved calendar widget

--- wideboy/sprites/calendar/__init__.py
+++ wideboy/sprites/calendar/__init__.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from pygame import Clock, Color, Event, Rect, Surface, SRCALPHA
 from wideboy.mqtt.homeassistant import HASS
 from wideboy.sprites.image_helpers import render_text
-from wideboy.constants import EVENT_EPOCH_MINUTE
+from wideboy.constants import EVENT_EPOCH_SECOND, EVENT_EPOCH_MINUTE
 from wideboy.sprites.base import BaseSprite
 
 
@@ -15,10 +15,12 @@ class CalendarSprite(BaseSprite):
         self,
         rect: Rect,
         entity_id: str,
-        event_count: int = 1,
+        event_count: int = 5,
         max_label_width: int = 64,
         font: str = "fonts/bitstream-vera.ttf",
         font_size: int = 8,
+        interval: int = 10,
+        days_ahead: int = 60,
     ) -> None:
         super().__init__(rect)
         self.entity_id = entity_id
@@ -26,8 +28,12 @@ class CalendarSprite(BaseSprite):
         self.max_label_width = max_label_width
         self.font = font
         self.font_size = font_size
+        self.interval = interval
+        self.days_ahead = days_ahead
         self.image = Surface((rect.width, rect.height), SRCALPHA)
         self.calendar_events = []
+        self.event_index = 0
+        self.update_events()
         self.render()
 
     def update(
@@ -40,37 +46,47 @@ class CalendarSprite(BaseSprite):
         super().update(frame, clock, delta, events)
         for event in events:
             if event.type == EVENT_EPOCH_MINUTE:
+                self.update_events()
+            if event.type == EVENT_EPOCH_SECOND and event.unit % self.interval == 0:
                 self.render()
+                self.event_index += 1
+                if self.event_index > len(self.calendar_events) - 1:
+                    self.event_index = 0
+
+    def update_events(self) -> None:
+        self.calendar_events = self.get_calendar_events()
+        self.event_index = 0
+
+    def render_event_text(self, event: dict) -> None:
+        start_date = event["start"]["date"]
+        ddmm_str = f"{start_date[8:10]}/{start_date[5:7]}"
+        label = self.truncate_label(event["summary"])
+        return render_text(
+            f"{ddmm_str} {label}",
+            self.font,
+            self.font_size,
+            Color(255, 255, 0),
+        )
 
     def render(self) -> None:
-        self.calendar_events = self.get_calendar_events()
-        rendered_events = []
-        for event in self.calendar_events[: self.event_count]:
-            start_date = event["start"]["date"]
-            ddmm_str = f"{start_date[8:10]}/{start_date[5:7]}"
-            label = self.truncate_label(event["summary"])
-            rendered_event = render_text(
-                f"{ddmm_str} {label}",
-                self.font,
-                self.font_size,
-                Color(255, 255, 0),
-            )
-            rendered_events.append(rendered_event)
-        self.image.fill((0, 0, 0, 0))
-        for i, rendered_event in enumerate(rendered_events):
-            self.image.blit(
-                rendered_event,
-                (
-                    (self.rect.width / 2) - (rendered_event.get_width() / 2),
-                    i * rendered_event.get_height(),
-                ),
-            )
+        if not len(self.calendar_events):
+            return
+        self.dirty = 1
+        event_surface = self.render_event_text(self.calendar_events[self.event_index])
+        self.image.fill(Color(0, 0, 0, 0))
+        self.image.blit(
+            event_surface,
+            (
+                (self.rect.width / 2) - (event_surface.get_width() / 2),
+                0,
+            ),
+        )
 
     def get_calendar_events(self) -> list[dict]:
         now = datetime.now()
-        next_year = now + timedelta(days=365)
+        then = now + timedelta(days=self.days_ahead)
         start = now.strftime("%Y-%m-%dT%H:%M:%S.000Z")
-        end = next_year.strftime("%Y-%m-%dT%H:%M:%S.000Z")
+        end = then.strftime("%Y-%m-%dT%H:%M:%S.000Z")
         events = HASS.request(f"calendars/{self.entity_id}?start={start}&end={end}")
         logger.debug(f"EVENTS {events}")
         return sorted(events, key=lambda event: event["start"]["date"])
