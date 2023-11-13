d7383088a8fbe99c3db1be89e3b4be10fb5c1eac
Louis King <jinglemansweep@gmail.com>
2023-11-13 23:21:22 +0000

    Stash

--- testing/sprites/__main__.py
+++ testing/sprites/__main__.py
@@ -25,8 +25,23 @@ class CellStates(enum.Enum):
     CLOSING = enum.auto()
 
 
+class CellCollapseStyle(enum.Enum):
+    HORIZONTAL = enum.auto()
+    VERTICAL = enum.auto()
+    BOTH = enum.auto()
+
+
 class BaseSprite(pygame.sprite.Sprite):
     pass
+
+
+class CellGroup(pygame.sprite.Group):
+    def update(self):
+        super().update()
+        cy = 0
+        for sprite in self.sprites():
+            sprite.rect.y = cy
+            cy += sprite.rect.height
 
 
 class Cell(BaseSprite):
@@ -36,39 +51,69 @@ class Cell(BaseSprite):
         y: int,
         width: int,
         height: int,
+        width_closed: int = 2,
         height_closed: int = 2,
         open: bool = True,
         color: pygame.Color = pygame.Color(0, 0, 0),
+        collapse_style: CellCollapseStyle = CellCollapseStyle.HORIZONTAL,
+        debug: bool = False,
     ):
         super().__init__()
         self.x = x
         self.y = y
-        self.width = width
+        self.width_open = width
+        self.width_closed = width_closed
+        self.width = self.width_open if open else self.width_closed
         self.height_open = height
         self.height_closed = height_closed
         self.height = self.height_open if open else self.height_closed
         self.open_state = CellStates.OPEN if open else CellStates.CLOSED
+        self.collapse_style = collapse_style
         self.color = color
+        self.debug = debug
         self.render(force_redraw=True)
 
     def update(self):
-        logger.debug(f"Cell.update: {self.open_state}")
+        if self.debug:
+            logger.debug(f"Cell.update: {self.open_state}")
         self.handle_animation()
         self.render()
 
     def handle_animation(self):
         if self.open_state == CellStates.OPENING:
-            self.height += 1
-            if self.height >= self.height_open:
-                self.height = self.height_open
+            if self.collapse_style in [
+                CellCollapseStyle.VERTICAL,
+                CellCollapseStyle.BOTH,
+            ]:
+                self.height += 1
+                if self.height >= self.height_open:
+                    self.height = self.height_open
+            if self.collapse_style in [
+                CellCollapseStyle.HORIZONTAL,
+                CellCollapseStyle.BOTH,
+            ]:
+                self.width += 1
+                if self.width >= self.width_open:
+                    self.width = self.width_open
+            if self.height == self.height_open and self.width == self.width_open:
                 self.open_state = CellStates.OPEN
-            logger.debug(f"Cell.handle_animation: OPENING {self.rect.height}")
         elif self.open_state == CellStates.CLOSING:
-            self.height -= 1
-            if self.height <= self.height_closed:
-                self.height = self.height_closed
+            if self.collapse_style in [
+                CellCollapseStyle.VERTICAL,
+                CellCollapseStyle.BOTH,
+            ]:
+                self.height -= 1
+                if self.height <= self.height_closed:
+                    self.height = self.height_closed
+            if self.collapse_style in [
+                CellCollapseStyle.HORIZONTAL,
+                CellCollapseStyle.BOTH,
+            ]:
+                self.width -= 1
+                if self.width <= self.width_closed:
+                    self.width = self.width_closed
+            if self.height == self.height_closed and self.width == self.width_closed:
                 self.open_state = CellStates.CLOSED
-            logger.debug(f"Cell.handle_animation: CLOSING {self.rect.height}")
 
     def render(self, force_redraw: bool = False):
         if force_redraw or self.open_state in [CellStates.OPENING, CellStates.CLOSING]:
@@ -76,11 +121,15 @@ class Cell(BaseSprite):
         self.draw_text()
 
     def toggle(self):
-        if self.open_state == CellStates.OPEN:
+        if self.open_state == CellStates.OPEN or self.open_state == CellStates.OPENING:
             self.open_state = CellStates.CLOSING
-        elif self.open_state == CellStates.CLOSED:
+        elif (
+            self.open_state == CellStates.CLOSED
+            or self.open_state == CellStates.CLOSING
+        ):
             self.open_state = CellStates.OPENING
-        logger.debug(f"Cell.toggle: {self.open_state}")
+        if self.debug:
+            logger.debug(f"Cell.toggle: {self.open_state}")
 
     def redraw_image(self, width: int, height: int):
         self.image = pygame.Surface((width, height))
@@ -104,16 +153,16 @@ class Cell(BaseSprite):
 clock = pygame.time.Clock()
 FPS = 50
 
-sprite_group: pygame.sprite.Group = pygame.sprite.Group()
+sprite_group: CellGroup = CellGroup()
 
 
-sprite1 = Cell(0, 0, 100, 50, color=pygame.Color(32, 0, 0, 255))
+sprite1 = Cell(0, 0, 100, 30, color=pygame.Color(128, 0, 0, 255))
 sprite_group.add(sprite1)
 
-sprite2 = Cell(100, 0, 100, 50, color=pygame.Color(0, 32, 0, 255))
+sprite2 = Cell(0, 50, 100, 30, color=pygame.Color(0, 128, 0, 255))
 sprite_group.add(sprite2)
 
-sprite3 = Cell(200, 0, 100, 50, color=pygame.Color(0, 0, 32, 255))
+sprite3 = Cell(0, 100, 100, 30, color=pygame.Color(0, 0, 128, 255))
 sprite_group.add(sprite3)
 
 
