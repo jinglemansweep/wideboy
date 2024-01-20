6fe83c6e23ae6b67d1cb5415eeae5eba4c6530e0
Louis King <jinglemansweep@gmail.com>
2024-01-20 15:08:06 +0000

    Optimising

--- wideboy/systems/display.py
+++ wideboy/systems/display.py
@@ -47,7 +47,7 @@ class SysDisplay(System):
             return
         render_surface = self.screen if app_state.master_power else self.screen_off
         self.buffer.SetImage(surface_to_led_matrix(render_surface))
-        self.matrix.SetBrightness((app_state.master_brightness / 255) * 100)
+        self.matrix.brightness = (app_state.master_brightness / 255) * 100
         self.matrix.SwapOnVSync(self.buffer)
 
     def _setup_matrix_driver(self) -> None:
