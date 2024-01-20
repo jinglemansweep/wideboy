4d58cebe3bba4f2bdfd1aad15cbd364cd2f08879
Louis King <jinglemansweep@gmail.com>
2024-01-20 12:27:29 +0000

    Fixed Pygame events, e.g. quitting

--- wideboy/systems/boot.py
+++ wideboy/systems/boot.py
@@ -4,9 +4,8 @@ from dynaconf import Dynaconf
 from ecs_pattern import EntityManager, System
 from pygame.constants import KEYDOWN, KEYUP, QUIT, K_UP, K_DOWN, K_ESCAPE
 from pygame.event import get as get_pygame_events
-from typing import Optional
-from ..entities import AppState
 from ..consts import EventTypes
+from ..entities import AppState
 
 logger = logging.getLogger(__name__)
 
@@ -31,16 +30,16 @@ class SysEvents(System):
 
     def start(self) -> None:
         logger.info("Events system starting...")
-        self.app_state = next(self.entities.get_by_class(AppState))
 
     def update(self) -> None:
         pygame_events = get_pygame_events()
-        self.app_state.events = [(event.type, event.dict) for event in pygame_events]
+        app_state = next(self.entities.get_by_class(AppState))
+        app_state.events = [(event.type, event.dict) for event in pygame_events]
         # logger.debug(
-        #     f"sys.events.update: events={len(self.events)} pygame_events={len(pygame_events)}"
+        #     f"sys.events.update: events={len(app_state.events)} pygame_events={len(pygame_events)}"
         # )
-        if len(self.app_state.events) > 0:
-            logger.debug(f"sys.events.update: {self.app_state.events}")
+        if len(app_state.events) > 0:
+            logger.debug(f"sys.events.update: {app_state.events}")
 
 
 class SysClock(System):
@@ -75,21 +74,16 @@ class SysInput(System):
     def __init__(self, entities: EntityManager) -> None:
         self.entities = entities
         self.event_types = (KEYDOWN, KEYUP, QUIT)  # Whitelist
-        self.app_state: Optional[AppState] = None
 
     def start(self) -> None:
         logger.info("Input system starting...")
-        self.app_state = next(self.entities.get_by_class(AppState))
 
     def update(self) -> None:
-        if not self.app_state:
-            return
-        for event in get_pygame_events(self.event_types):
-            event_type = event.type
-            event_key = getattr(event, "key", None)
-            # Quit App
+        app_state = next(self.entities.get_by_class(AppState))
+        for event_type, event_payload in app_state.events:
+            event_key = event_payload.get("key", None)
             if (event_type == KEYDOWN and event_key == K_ESCAPE) or event_type == QUIT:
-                self.app_state.running = False
+                app_state.running = False
             # Up/Down
             if event_type == KEYDOWN and event_key in (K_UP, K_DOWN):
                 logger.debug("Up/Down", event_key)
