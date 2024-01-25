fa41ecc1054af9473b4cf8f25531b4b54e809f2b
Louis King <jinglemansweep@gmail.com>
2024-01-25 00:41:53 +0000

    Added some cheesey transitions

--- wideboy/systems/scene/stages/default.py
+++ wideboy/systems/scene/stages/default.py
@@ -101,7 +101,11 @@ class StageDefault(Stage):
             self.app_state.slideshow_index = 0
         next_image = load_image(self.slideshow_images[self.app_state.slideshow_index])
         widget_slideshow.sprite.set_next_image(next_image)
-        widget_slideshow.sprite.swap(random.choice([Transition.BLEED]))
+        widget_slideshow.sprite.swap(
+            random.choice(
+                [Transition.FADE, Transition.WIPE, Transition.FOLD, Transition.BLEED]
+            )
+        )
 
     def _glob_backgrounds(self, randomize: bool = False) -> None:
         app_state = next(self.entities.get_by_class(AppState))
