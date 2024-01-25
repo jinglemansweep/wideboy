844f10e9dd098575e9dbe260c352cfbeb66cb11f
Louis King <jinglemansweep@gmail.com>
2024-01-25 18:54:19 +0000

    Trippy grid

--- wideboy/systems/preprocess.py
+++ wideboy/systems/preprocess.py
@@ -160,13 +160,13 @@ class SysPreprocess(System):
             preprocess_mode7(
                 self.cache,
                 "mode7_tron_grid",
-                f"{self.app_state.config.paths.images_sprites}/misc/grid_red.png",
+                f"{self.app_state.config.paths.images_sprites}/misc/grid_squares_color.png",
                 (
                     self.display_info.current_w,
                     self.display_info.current_h,
                 ),
-                0.1,
+                0.05,
                 0 - r,
-                0.8,
+                0.9,
             )
             yield f"Tron Grid [{r}]"
